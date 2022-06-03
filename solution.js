c5c9eb9949d81153bd47220d2f358a584b1711a0
Adnan Ather <99840210+elgorhythm@users.noreply.github.com>
2022-06-03 08:44:58 -0600

    Update solution.js

--- solution.js
+++ solution.js
@@ -1,1 +1,17 @@
-#solution code
+function animals(heads, legs){
+  
+let chicken
+let cows
+let anims=[]
+
+for (i=0; i<=heads; i++) {
+  if ((legs-(i*2))%4===0 && (i*2+(heads-i)*4)===legs) {
+    chicken = i
+    cows = heads-i
+    anims = [i,heads-i]
+    return anims
+    break   
+    }
+  }
+  return "No solutions"
+}
